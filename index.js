a2abb5e0590afce78f670c017c8e68c598bbdaf4
Martin Hula <hulamart@gmail.com>
2020-10-11 23:03:45 +0200

    different lib

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+import { des } from './des.js';
+
+/*
 import CryptoJS from 'crypto-js';
 
 // 16 rounds
-var encrypted = CryptoJS.DES.encrypt("Message", "Secret Passphrase");
+var encrypted = CryptoJS.DES.encrypt("Message", "5555555555555555");
 
 console.log(encrypted.toString());
 
-var decrypted = CryptoJS.DES.decrypt(encrypted, "Secret Passphrase");
+var decrypted = CryptoJS.DES.decrypt(encrypted, "5555555555555555");
 
 console.log(decrypted.toString()); // in HEX for some reason
 
@@ -20,6 +23,7 @@ var decrypted = CryptoJS.DES.decrypt(encrypted, "Secret Passphrase", { rounds: 8
 
 console.log(decrypted.toString()); // in HEX for some reason
 
+*/
 
 // 16 rounds
 // U2FsdGVkX1+fSQqEny7pV1nJ7K2y9Q3e
@@ -28,3 +32,63 @@ console.log(decrypted.toString()); // in HEX for some reason
 // 8 rounds
 // U2FsdGVkX19DQiH70LIHJskMs741FLE1
 // 4d657373616765
+
+
+let globalKey = '14c79f4e31d03b4c';
+let globalVector = '';//'bda20c45101a615f';
+
+let encryptString = "Message";
+let decryptString = "b7f1117111ebf530";
+let result = '';
+
+//encrypt_string();
+decrypt_string();
+
+console.log(result);
+
+// end key generator stuff
+function chars_from_hex(inputstr) {
+	var outputstr = '';
+	inputstr = inputstr.replace(/^(0x)?/g, '');
+	inputstr = inputstr.replace(/[^A-Fa-f0-9]/g, '');
+	inputstr = inputstr.split('');
+	for(var i=0; i<inputstr.length; i+=2) {
+		outputstr += String.fromCharCode(parseInt(inputstr[i]+''+inputstr[i+1], 16));
+	}
+	return outputstr;
+}
+function hex_from_chars(inputstr) {
+	var delimiter = '';
+	var outputstr = '';
+	var hex = "0123456789abcdef";
+	hex = hex.split('');
+	var i, n;
+	var inputarr = inputstr.split('');
+	for(var i=0; i<inputarr.length; i++) {
+		if(i > 0) outputstr += delimiter;
+		if(!delimiter && i % 32 == 0 && i > 0) outputstr += '\n';
+		n = inputstr.charCodeAt(i);
+		outputstr += hex[(n >> 4) & 0xf] + hex[n & 0xf];
+	}
+	return outputstr;
+}
+function encrypt_string() {
+    var input = encryptString;
+    var key = globalKey;
+    key = chars_from_hex(key);
+    var vector = globalVector;
+    vector = chars_from_hex(vector);
+    var vector = (vector.length > 7) ? vector : null;
+    var output = des(key, input, 1, vector ? 1 : 0, vector);
+    result = hex_from_chars(output);
+}
+function decrypt_string() {
+    var input = decryptString;
+    var key = globalKey;
+    key = chars_from_hex(key);
+    var vector = globalVector;
+    vector = chars_from_hex(vector);
+    var vector = (vector.length > 7) ? vector : null;
+    input = chars_from_hex(input);
+    result = des(key, input, 0, vector ? 1 : 0, vector);
+}
